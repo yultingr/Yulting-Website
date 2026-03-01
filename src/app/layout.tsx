722b12d44121397bdc2dd1aa09aa9d99a12501ad
Yulting Tulku <yultingtulku@Yultings-MacBook-Pro.local>
2026-03-02 00:44:36 +0530

    Revert "Add Saffron & Maroon design overhaul with Tibetan decorative elements"
    This reverts commit eac9977610bb1ce155452f1effc67437a09e78dd.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,9 @@
-import { Inter, EB_Garamond } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
-});
-
-const ebGaramond = EB_Garamond({
-  variable: "--font-serif",
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
 });
 
 export default function RootLayout({
@@ -19,9 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html suppressHydrationWarning>
-      <body
-        className={`${inter.variable} ${ebGaramond.variable} font-sans antialiased`}
-      >
+      <body className={`${inter.variable} font-sans antialiased`}>
         {children}
       </body>
     </html>
